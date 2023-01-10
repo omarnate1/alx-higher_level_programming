8db0aee879d5ef80db0521ed0feb731e56cde19a
omarnate1 <omarnate@gmail.com>
2023-01-10 13:39:09 +0000

    task done

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
+"""Module 0-lookup
+Returns a list of the attributes and methods of an object
+"""
 
 
 def lookup(obj):
-    """
-    Returns all objects in an objects dictionary as a list
+    """Returns the list of the attributes and methods of obj
+    Args:
+        obj (_type_): an object to be used
+    Returns:
+        dir(obj)
     """
     return dir(obj)
