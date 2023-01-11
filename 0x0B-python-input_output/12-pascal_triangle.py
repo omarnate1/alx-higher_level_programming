04cf40b0deb72c4643b676c162f72c323a9a5e64
omarnate1 <omarnate@gmail.com>
2023-01-11 06:43:55 +0000

    task done

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tru Jan 11 15:13:37 2023
+@author: Nathan Orobor
+"""
+
+
+def pascal_triangle(n):
+    """
+    Creates a pascal triangle
+    Attributes:
+        n (int): The n exponent for triangle
+    Return:
+        A matrix with values for the triangle
+    """
+    pascal = []
+    triangle = []
+
+    for i in range(int(n)):
+        new = pascal[:]
+        new.append(1)
+        pos = len(pascal)
+        for i in range(1, pos):
+            new[i] = pascal[i - 1] + pascal[i]
+        pascal = new[:]
+        triangle.append(pascal)
+    return 
